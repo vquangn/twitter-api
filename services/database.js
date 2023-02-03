e5b7aafbb5d0e11f4595f2fa1fd19a77b7faa461
vquangn <110308585+vquangn@users.noreply.github.com>
2023-02-03 11:29:24 +0100

    postres_render

--- services/database.js
+++ services/database.js
@@ -1,11 +1,18 @@
 const {Pool} = require('pg');
 
+const POSTGRES_URL =
+	'postgresql://twitterdb_nf24_user:WYGVFFROb4M0tS6muLsOzohSf9qo0euE@dpg-cfedjr4gqg46rpnkbp70-a:5432/twitterdb_nf24';
+
+// const database = new Pool({
+// 	user: 'postgres',
+// 	host: 'localhost',
+// 	database: 'twitter',
+// 	password: '',
+// 	port: 5432,
+// });
+
 const database = new Pool({
-	user: 'postgres',
-	host: 'localhost',
-	database: 'twitter',
-	password: '',
-	port: 5432,
+	connectionString: POSTGRES_URL,
 });
 
 async function getTweets() {
